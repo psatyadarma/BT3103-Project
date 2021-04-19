1af4c6be1846f7ea739a322770181b81a25c9890
Hari <skcharis77@gmail.com>
2021-04-19 23:02:41 +0800

    Style RegisterTutor

--- src/components/RegisterTutor.vue
+++ src/components/RegisterTutor.vue
@@ -1,96 +1,184 @@
 <template>
-  <div style="text-align:center" class="formdiv">
-    <h1>Register Your Account</h1><br>
-    <form @submit.prevent="register">
-      
-      <label for="first" style="font-size:16pt">First Name</label><br>
-      <input
-        type="text"
-        placeholder="First Name"
-        v-model="first"
-      required /><br><br>
-      <label for="last" style="font-size:16pt">Last Name</label><br>
-      <input
-        type="text"
-        placeholder="Last Name"
-        v-model="last"
-      required /><br><br>
-      <label for="phone" style="font-size:16pt">Phone Number</label><br>
-      <input 
-        v-model.number="phone" 
-        type="number"
-        placeholder="Phone Number"
-      required /><br><br>
-      <label for="qualifications" style="font-size:16pt">Your Qualifications</label><br>
-      <textarea 
-        v-model="qualifications" 
-        placeholder="Your qualifications"
-      ></textarea><br>
-      <p style="font-size:16pt">Teaching Level</p>
-      <input type="checkbox" id="primary" value="Primary" v-model="level" class="check">
-      <label for="primary">Primary</label>
-      <input type="checkbox" id="secondary" value="Secondary" v-model="level" class="check">
-      <label for="secondary">Secondary</label>
-      <input type="checkbox" id="jc" value="JC" v-model="level" class="check">
-      <label for="jc">JC</label>
-      <input type="checkbox" id="university" value="University" v-model="level" class="check">
-      <label for="university">University</label>
-      <br><br><br>
-      <label for="experience" style="font-size:16pt">Years of Experience</label><br>
-      <input 
-        v-model.number="experience" 
-        type="number"
-      min="0" max="60" oninput="validity.valid||(value='');"/><br><br>
-      <p style="font-size:16pt">Teaching Subjects</p>
-      <input type="checkbox" id="mathematics" value="Mathematics" v-model="subject" class="check">
-      <label for="mathematics">Mathematics</label>
-      <input type="checkbox" id="english" value="English" v-model="subject" class="check">
-      <label for="english">English</label>
-      <input type="checkbox" id="physics" value="Physics" v-model="subject" class="check">
-      <label for="physics">Physics</label>
-      <input type="checkbox" id="chemistry" value="Chemistry" v-model="subject" class="check">
-      <label for="chemistry">Chemistry</label>
-      <input type="checkbox" id="biology" value="Biology" v-model="subject" class="check">
-      <label for="biology">Biology</label>
-      <br><br><br>
-      <label for="rates" style="font-size:16pt">Rates</label><br>
-      <input 
-        v-model.number="rates" 
-        type="number"
-        placeholder="Hourly Rate"
-      min=0 oninput="validity.valid||(value='');" /><br><br>
-      <label for="qualifications" style="font-size:16pt">Your Available Days</label><br>
-      <textarea 
-        v-model="availability" 
-        placeholder="Your available days"
-      required ></textarea><br><br>
-      <label for="email" style="font-size:16pt">Email Address</label><br>
-      <input
-        type="email"
-        placeholder="Email address"
-        v-model="email"
-      required /><br><br>
-      <label for="password" style="font-size:16pt">Password</label><br>
-      <input
-        type="password"
-        placeholder="Password"
-        v-model="password"
-      required /><br><br>
-      <button type="submit" value="register">Register</button>
-    </form>
-</div>
+  <div>
+    <img id="logo" :src="logo" />
+    <nav>
+      <ul class="navbar" style="list-style-type: none;">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/login">Login</router-link></li>
+        <li><router-link to="/logout">Logout</router-link></li>
+      </ul>
+    </nav>
+    <div>
+      <h1>Register Your Account</h1>
+    </div>
+    <div style="text-align:center" class="formdiv">
+      <form @submit.prevent="register">
+        <label for="first" >First Name</label><br />
+        <input
+          type="text"
+          placeholder="First Name"
+          v-model="first"
+          required
+        /><br /><br />
+        <label for="last" >Last Name</label><br />
+        <input
+          type="text"
+          placeholder="Last Name"
+          v-model="last"
+          required
+        /><br /><br />
+        <label for="phone" >Phone Number</label><br />
+        <input
+          v-model.number="phone"
+          type="number"
+          placeholder="Phone Number"
+          required
+        /><br /><br />
+        <label for="qualifications" 
+          >Your Qualifications</label
+        ><br />
+        <textarea
+          v-model="qualifications"
+          placeholder="Your qualifications"
+        ></textarea
+        ><br />
+        <p >Teaching Level</p>
+        <input
+          type="checkbox"
+          id="primary"
+          value="Primary"
+          v-model="level"
+          class="check"
+        />
+        <label for="primary">Primary</label>
+        <input
+          type="checkbox"
+          id="secondary"
+          value="Secondary"
+          v-model="level"
+          class="check"
+        />
+        <label for="secondary">Secondary</label>
+        <input
+          type="checkbox"
+          id="jc"
+          value="JC"
+          v-model="level"
+          class="check"
+        />
+        <label for="jc">JC</label>
+        <input
+          type="checkbox"
+          id="university"
+          value="University"
+          v-model="level"
+          class="check"
+        />
+        <label for="university">University</label>
+        <br /><br /><br />
+        <label for="experience" 
+          >Years of Experience</label
+        ><br />
+        <input
+          v-model.number="experience"
+          type="number"
+          min="0"
+          max="60"
+          oninput="validity.valid||(value='');"
+        /><br /><br />
+        <p >Teaching Subjects</p>
+        <input
+          type="checkbox"
+          id="mathematics"
+          value="Mathematics"
+          v-model="subject"
+          class="check"
+        />
+        <label for="mathematics">Mathematics</label>
+        <input
+          type="checkbox"
+          id="english"
+          value="English"
+          v-model="subject"
+          class="check"
+        />
+        <label for="english">English</label>
+        <input
+          type="checkbox"
+          id="physics"
+          value="Physics"
+          v-model="subject"
+          class="check"
+        />
+        <label for="physics">Physics</label>
+        <input
+          type="checkbox"
+          id="chemistry"
+          value="Chemistry"
+          v-model="subject"
+          class="check"
+        />
+        <label for="chemistry">Chemistry</label>
+        <input
+          type="checkbox"
+          id="biology"
+          value="Biology"
+          v-model="subject"
+          class="check"
+        />
+        <label for="biology">Biology</label>
+        <br /><br /><br />
+        <label for="rates" >Rates</label><br />
+        <input
+          v-model.number="rates"
+          type="number"
+          placeholder="Hourly Rate"
+          min="0"
+          oninput="validity.valid||(value='');"
+        /><br /><br />
+        <label for="qualifications" 
+          >Your Available Days</label
+        ><br />
+        <textarea
+          v-model="availability"
+          placeholder="Your available days"
+          required
+        ></textarea
+        ><br /><br />
+        <label for="email" >Email Address</label><br />
+        <input
+          type="email"
+          placeholder="Email address"
+          v-model="email"
+          required
+        /><br /><br />
+        <label for="password" >Password</label><br />
+        <input
+          type="password"
+          placeholder="Password"
+          v-model="password"
+          required
+        /><br /><br />
+        <button type="submit" value="register">Register</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-import firebase from "../firebase"
+
+import logo from "../assets/logo2.png"
+import firebase from "../firebase";
 var db = firebase.firestore();
 
 var pastThirtyDays = [];
-for (let i=0; i < 30; i++) {
-  var date = new Date(new Date().setDate(new Date().getDate() - i)).toDateString().slice(4);
+for (let i = 0; i < 30; i++) {
+  var date = new Date(new Date().setDate(new Date().getDate() - i))
+    .toDateString()
+    .slice(4);
   pastThirtyDays.push(date);
 }
-pastThirtyDays = pastThirtyDays.sort((a,b) => new Date(a) - new Date(b));
+pastThirtyDays = pastThirtyDays.sort((a, b) => new Date(a) - new Date(b));
 
 var clickHistory = {};
 var viewHistory = {};
@@ -101,27 +189,28 @@ for (const date of pastThirtyDays) {
 }
 
 export default {
-  name: 'RegisterTutor',
+  name: "RegisterTutor",
   data() {
     return {
-      first:'',
-      last:'',
-      email: '',
-      password: '',
-      qualifications:'',
-      subject:[],
-      rates:'',
-      phone:'',
-      availability:'',
-      experience:'',
-      image:'',
-      level:[],
-      engaging:[0,0,0,0,0,0,0,0,0,0],
-      communication:[0,0,0,0,0,0,0,0,0,0],
-      listening:[0,0,0,0,0,0,0,0,0,0],
-      patience:[0,0,0,0,0,0,0,0,0,0],
-      overall:[0,0,0,0,0]
-    }
+      first: "",
+      last: "",
+      email: "",
+      password: "",
+      qualifications: "",
+      subject: [],
+      rates: "",
+      phone: "",
+      availability: "",
+      experience: "",
+      image: "",
+      level: [],
+      engaging: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      communication: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      listening: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      patience: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      overall: [0, 0, 0, 0, 0],
+      logo: logo
+    };
   },
   methods: {
     register() {
@@ -131,68 +220,92 @@ export default {
         .then((user) => {
           var tutorid = user.user.uid;
           console.log(tutorid);
-          db.collection("profiles").doc(user.user.uid).set({
-            first_name: this.first,
-            last_name: this.last,
-            email: this.email,
-            qualifications: this.qualifications,
-            subject: this.subject,
-            rates: this.rates,
-            phone:this.phone,
-            rate:0,
-            tutid:tutorid,
-            availability:this.availability,
-            experience:this.experience,
-            image:this.image,
-            level:this.level,
-            engaging:this.engaging,
-            communication:this.communication,
-            listening:this.listening,
-            patience:this.patience,
-            overall:this.overall,
-            profileViews:0,
-            contactClicks:0,
-            createDate: new Date(),
-            clickHistory: clickHistory,
-            viewHistory: viewHistory,
-            mystudents: [],
-          })
-          .then(function() {
-            console.log("Document successfully written!");
-          })
-          .catch(function(error) {
-             console.error("Error writing document: ", error);
-          });
-          alert('Successfully registered!');
-          
-          this.$router.push('/homeTutor');
+          db.collection("profiles")
+            .doc(user.user.uid)
+            .set({
+              first_name: this.first,
+              last_name: this.last,
+              email: this.email,
+              qualifications: this.qualifications,
+              subject: this.subject,
+              rates: this.rates,
+              phone: this.phone,
+              rate: 0,
+              tutid: tutorid,
+              availability: this.availability,
+              experience: this.experience,
+              image: this.image,
+              level: this.level,
+              engaging: this.engaging,
+              communication: this.communication,
+              listening: this.listening,
+              patience: this.patience,
+              overall: this.overall,
+              profileViews: 0,
+              contactClicks: 0,
+              createDate: new Date(),
+              clickHistory: clickHistory,
+              viewHistory: viewHistory,
+              mystudents: [],
+            })
+            .then(function() {
+              console.log("Document successfully written!");
+            })
+            .catch(function(error) {
+              console.error("Error writing document: ", error);
+            });
+          alert("Successfully registered!");
+
+          this.$router.push("/homeTutor");
         })
-        .catch(error => {
+        .catch((error) => {
           alert(error.message);
         });
+    },
   },
-},
-}
+};
 </script>
 
 <style scoped>
+
 h1 {
-  font-size: 24pt
+    text-align: center;
+    font-size: 64px;
+    padding-top: 100px;
+    margin: 10px auto;
+}
+
+#logo {
+  float: left;
+  padding-left:20px;
+  padding-top: 15px;
+  height: 100px;
+  width: 95px;
+  top:50px;
 }
 
 .formdiv {
   border-radius: 25px;
   border: 4px solid black;
   padding: 20px;
-  background-image:linear-gradient(#55C9C2, white);
-  width: 550px;
-  margin: auto;
+  background-image: linear-gradient(#55c9c2, white);
+  width: 700px;
+  margin: 70px auto;
   border-style: outset;
-  font-weight : bold;
+  font-weight: bold;
 }
 
-input[type=checkbox]
-{
+label {
+    margin: 10px auto;
+    font-size: 16pt;
+}
+
+p {
+    margin: 15px auto;
+    font-size: 16pt;
+}
+
+input[type="checkbox"] {
   /* Double-sized Checkboxes */
   -ms-transform: scale(2); /* IE */
   -moz-transform: scale(2); /* FF */
@@ -205,41 +318,73 @@ input[type=checkbox]
 }
 
 /* Might want to wrap a span around your checkbox text */
-.checkboxtext
-{
+.checkboxtext {
   /* Checkbox text */
   font-size: 18pt;
   display: inline;
 }
 
-input:not(.check), select {
-  height:40px;
-  width:200 px;
+input:not(.check),
+select {
+  height: 40px;
+  width: 200 px;
   font-size: 14pt;
-  margin:5px 0;
-  padding:8px;
-  border-radius:10px;
-  box-shadow: 2px 1px
+  margin: 5px 0;
+  padding: 8px;
+  border-radius: 10px;
+  box-shadow: 2px 1px;
 }
 
 textarea {
   height: 60px;
   width: 240px;
   font-size: 14pt;
-  margin:5px 0;
-  padding:8px;
-  border-radius:10px;
-  box-shadow: 2px 1px
+  margin: 5px 0;
+  padding: 8px;
+  border-radius: 10px;
+  box-shadow: 2px 1px;
 }
 
 button {
-  height:40px;
-  width:100px;
-  font-size: 14pt;
-  background-color:aquamarine
+    font-family: Montserrat;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: normal;
+    background: #50cdc5;
+    border-radius: 20px;
+    padding: 7px 35px;
+    background: #3a938d;
 }
 
-input,textarea:focus { 
-  outline: none; 
+input,
+textarea:focus {
+  outline: none;
 }
+
+nav {
+  list-style-type: none;
+  margin: 10px;
+  padding: 0;
+  overflow: hidden;
+  color: black;
+  float: right;
+  display: block;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+nav li {
+  float: left;
+}
+
+nav a {
+  display: block;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-weight: bold;
+}
+
 </style>
