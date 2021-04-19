dedbdcd5144ef7d101099376d78cf33763510532
simxl <71934909+simxl@users.noreply.github.com>
2021-04-20 01:08:43 +0800

    removed uid

--- src/components/AssignmentTutor_ToGrade.vue
+++ src/components/AssignmentTutor_ToGrade.vue
@@ -48,7 +48,7 @@ export default {
   data() {
       return {
           logo: logo,
-          thisUserId: "BKtdsQm4OmQO8UwPe1ktIwUqhgb2", //firebase.auth().currentUser.uid,
+          thisUserId: firebase.auth().currentUser.uid,
           allAssignments: [],
           sorted:[], 
       };
