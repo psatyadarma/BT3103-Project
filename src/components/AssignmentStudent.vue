bf395bec41c7c2d963669ff673a330329f571380
zeriw <71934909+simxl@users.noreply.github.com>
2021-04-19 17:06:54 +0800

    Input new functions
    - sort assignment by date
    - added trash function to delete assignment

--- src/components/AssignmentStudent.vue
+++ src/components/AssignmentStudent.vue
@@ -1,19 +1,25 @@
 <template>
     <div id='full'>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <button id='assgn'>Assignment</button>
+        <button id='upload' @click="$router.push('/uploadStudent')">Upload File</button>
         
         <div id='headings'>
             <label id='dueDate'>Due Date</label>
             <label id='subject'>Subject</label>
             <label id='assignment'>Assignment</label>
-            <input type='checkbox' id='checkboxFilter' v-model='completed'>
             <hr id='linebreak'>
         </div>
 
         <div id='assignmentList'>
             <ul>
-                <li v-for="assgn in allAssignments" :key="assgn.dueDate">
-                    {{assgn.dueDate}} {{assgn.subject}} {{assgn.header}}
+                <li v-for="assgn in sortedAssignments" :key="generateKey(assgn.dueDate, assgn.id)">
+                    <span id='dateList'>{{assgn.dueDate}} </span>
+                    <span id='subjectList'>{{assgn.subject}} </span>
+                    <span id='headerList'>{{assgn.header}} </span>
+                    <span id='descList'>{{assgn.description}} </span>
+                    <button id='downloadBtn' @click.prevent="download(assgn.uploadURL, assgn.header)"><i class="fa fa-download"></i></button>
+                    <button id='trashBtn' @click.prevent="deleteFile(assgn.id,assgn.uploadURL)"><i class="fa fa-trash"></i></button>
                 </li>
             </ul>
         </div>
@@ -30,21 +36,29 @@ export default {
   name: 'AssignmentStudent',
   data() {
       return {
+          thisUserId: "z0CCpM0ydJPwz8Q4H6We2fYem7t1", //firebase.auth().currentUser.uid,
           allAssignments: [],
       };
   },
+  computed: {
+      sortedAssignments: function() {
+          return this.allAssignments.sort((a,b) => a.dateObject - b.dateObject)
+      }
+  },
   methods: {
       fetchAssignments: function() {
-          db.collection('student_files').where("studentId", "==", firebase.auth().currentUser.uid).get().then((querySnapShot)=> {
+          db.collection('student_files').where("student", "==", this.thisUserId).get().then((querySnapShot)=> {
             let assignment = {}
             querySnapShot.forEach((doc) => {
                   assignment = doc.data()
+                  assignment.id = doc.id
+                  assignment.dateObject = new Date(assignment.dueDate)
                   this.allAssignments.push(assignment)
             })
           })
       },
 
-      download: function(link) {
+      download: function(link, header) {
           const storageRef = firebase.storage().ref();
           storageRef.child(link).getDownloadURL()
               .then((url) => {
@@ -55,7 +69,7 @@ export default {
                   var blob = xhr.response;
                   const link = document.createElement('a');
                   link.href = URL.createObjectURL(blob);
-                  link.download = this.header;
+                  link.download = header;
                   link.click();
                   URL.revokeObjectURL(link.href);
                 };
@@ -63,10 +77,36 @@ export default {
                 xhr.send();
             })
       },
+
+      deleteFile: function(id, link) {
+          this.removeFileFromFirestore(id)
+          this.removeFileFromFirebase(link)
+          location.reload()
+          this.fetchAssignments()
+      },
+
+      removeFileFromFirestore: function(id) {
+        db.collection('student_files').doc(id).delete().then(() => {
+            console.log('Document deleted successfully from firestore')
+        })
+      },
+
+      removeFileFromFirebase: function(link) {
+        const storageRef = firebase.storage().ref();
+        storageRef.child(link).delete().then(() => 
+        console.log('File deleted sucessfully from firebase'))
+      },
+
+      generateKey(date, id) {
+      const uniqueKey = `${date}-${id}`;
+      return uniqueKey;
+      }
+
   },
 
   created() {
       this.fetchAssignments()
+      console.log("all assignments: ", this.allAssignments)
   },
 
 }
@@ -102,6 +142,24 @@ export default {
     font-weight: 500;
     font-size: 24px;
     color: #FFFFFF;
+}
+
+#upload {
+    position: absolute;
+    width: 230px;
+    height: 52px;
+    right: 30px;
+    top: 25px;
+
+    background: #FFFFFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 51px;
+
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    color: #000000;
 }
 
 #record {
@@ -148,10 +206,27 @@ export default {
     width: 1750%;
 }
 
-#checkboxFilter {
-    transform: scale(2);
+input[type=checkbox] {
+    transform: scale(1.6);
+    vertical-align: middle;
 }
 
+#assignmentList {
+    position: absolute;
+    top: 120px;
+    font-size: 20px;
+}
 
+ul {
+    overflow-y:scroll;
+}
+
+ul li {
+    background: #ffffff;
+    margin: 10px;
+    padding: 5px;
+    border-radius: 20px;
+    width: 1320px;
+}
 
 </style>
